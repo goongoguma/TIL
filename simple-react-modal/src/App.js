a6cc36525f1d8b76a6bf0147fc92d4f1534857ed
JAEHYUN AN <jaehyun.an@purpleworks.co.kr>
2021-01-22 14:16:36 +0900

    포탈을 이용한 리액트 모달

---
+++ simple-react-modal/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Modal from './Modal';
+
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
+};
+
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
+};
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+ return (
+   <>
+    <div style={BUTTON_WRAPPER_STYLES}>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        Fancy Modal
+      </Modal>
+    </div>
+
+    <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+   </>
+ )
+}
+
+export default App;
