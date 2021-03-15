5140dc47c9c551980ec432b11bdc6870b617b5fc
JAEHYUN AN <jaehyun.an@purpleworks.co.kr>
2021-03-15 17:18:59 +0900

    가로스크롤

--- react-horizontal-scroll/src/App.js
+++ react-horizontal-scroll/src/App.js
@@ -1,23 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='outer-wrapper'>
+      <div className='wrapper'>
+        <section className='slide one'>
+          <div className='line'>
+            <>
+              <input type='checkbox' id='dot-btn1' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn1' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn2' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn2' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn3' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn3' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn4' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn4' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+          </div>
+        </section>
+        <section className='slide two'>
+          <div className='line'>
+          <>
+              <input type='checkbox' id='dot-btn5' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn5' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn6' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn6' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn7' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn7' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn8' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn8' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+          </div>
+        </section>
+        <section className='slide three'>
+          <div className='line'>
+          <>
+              <input type='checkbox' id='dot-btn9' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn9' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn10' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn10' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn11' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn11' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn12' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn12' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+          </div>
+        </section>
+        <section className='slide four'>
+          <div className='line'>
+          <>
+              <input type='checkbox' id='dot-btn13' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn13' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn14' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn14' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn15' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn15' />
+              <div className='comments-down'>
+                <div className='comment-down bubble-down'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+            <>
+              <input type='checkbox' id='dot-btn16' style={{display: 'none'}} />
+              <label className='dot-label' htmlFor='dot-btn16' />
+              <div className='comments-up'>
+                <div className='comment-up bubble-up'>
+                  this is a speech bubble
+                </div>
+              </div>
+            </>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
