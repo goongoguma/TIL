1e5c77f79abcc9a70b36c4529f790a643bb7cc4f
JaeHyun <wogus7an@gmail.com>
2022-01-18 08:32:27 +0900

    파일수정

--- hamburger/SWRQueryKeys.ts
+++
@@ -1,9 +0,0 @@
-const SWRQueryKeys = {
-  getAccountList: "GET_ACCOUNT_LIST" as const,
-  getAccount: "GET_ACCOUNT" as const,
-  getGuidelineList: "GET_GUIDELINE_LIST" as const,
-  getGuideline: "GET_GUIDELINE" as const,
-  getGuidelinePage: "GET_GUIDELINE_PAGE" as const,
-};
-
-export default SWRQueryKeys;
