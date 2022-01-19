7b9910ad281927f48dd80dd9b8542e59c3ec8b4e
JaeHyun <wogus7an@gmail.com>
2022-01-19 11:18:12 +0900

    integrate react-query

---
+++ react-query-tutorial/src/components/RQSuperHeroes.page.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+const RQSuperHeroesPage = () => {
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  );
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQSuperHeroes</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
+
+export default RQSuperHeroesPage;
