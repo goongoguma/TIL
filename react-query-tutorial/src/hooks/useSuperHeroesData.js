f536419b3d3837b3d238aa4a0ffe0a56545f543f
JaeHyun <wogus7an@gmail.com>
2022-01-24 09:55:31 +0900

    invalidate query

--- react-query-tutorial/src/hooks/useSuperHeroesData.js
+++ react-query-tutorial/src/hooks/useSuperHeroesData.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
@@ -36,5 +36,8 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 };
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero);
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: () => queryClient.invalidateQueries("super-heroes"),
+  });
 };
